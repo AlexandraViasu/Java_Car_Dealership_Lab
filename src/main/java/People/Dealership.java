3404154aab9bfd9db5bce36accccf0e3be48b5ec
AlexandraViasu <viasu.alexandra@gmail.com>
2023-08-22 14:04:31 +0100

    finishing touches

--- src/main/java/People/Dealership.java
+++ src/main/java/People/Dealership.java
@@ -1,4 +1,30 @@
-package People;
+package people;
+
+import Vehicles.Vehicle;
+
+import java.util.ArrayList;
 
 public class Dealership {
+
+    private ArrayList<Vehicle> vehicles;
+    private String name;
+    private double till;
+
+    public Dealership(ArrayList<Vehicle> vehicles, String name, double till) {
+        this.vehicles = vehicles;
+        this.name = name;
+        this.till = till;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int vehicleCount(){
+        return this.vehicles.size();
+    }
+
+    public double getTillBalance() {
+        return this.till;
+    }
 }
--- src/test/java/Vehicles/CarTest.java
+++
@@ -1,7 +0,0 @@
-package Vehicles;
-
-
-
-class CarTest {
-
-}
